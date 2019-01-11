b45b7214077c403f7d8bb9110e651e381626429c
camalzade_elvin <camalzade_elvin@outlook.com>
2019-01-11 20:35:58 +0400

    clocks intersect it has a problem

--- ClockProjectAsDigital/Form1.cs
+++ ClockProjectAsDigital/Form1.cs
@@ -7,13 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace ClockProjectAsDigital
 {
     public partial class Form1 : Form
     {
-        DateTime london_time = DateTime.Now.AddHours(-4);
-        DateTime baku_time = DateTime.Now;
+        public DateTime london_time { get; set; }
+        public DateTime baku_time { get; set; }
         public Label MyClockLabel { get; set; }
         public string imagepath { get; set; }
         public string LabelImage { get; set; }
@@ -22,41 +21,49 @@ namespace ClockProjectAsDigital
             InitializeComponent();
         }
         private void Form1_Load(object sender, EventArgs e)
-        {            
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Start();
+            timer.Tick += Timer_Tick;
             Label clocklabel = new Label();
             clocklabel.Location = new Point(170, 30);
             clocklabel.BackColor = Color.Blue;
             clocklabel.Size = new Size(500, 350);
-            clocklabel.Text = baku_time.ToLongTimeString();
-            clocklabel.Font = new Font("Italic", 50);
+            clocklabel.Text = DateTime.Now.ToLongTimeString();
+            clocklabel.Font = new Font("Imprint MT Shadow", 50);
             clocklabel.Image = Image.FromFile("baku.png");
-            Button baku_clock = new Button();
-            Button london_clock = new Button();
-            baku_clock.Size = new Size(85, 30);
-            london_clock.Size = new Size(85, 30);
-            baku_clock.Location = new Point(250, 150);
-            london_clock.Location = new Point(400, 150);
-            baku_clock.BackColor = Color.LightBlue;
-            london_clock.BackColor = Color.LightBlue;
-            baku_clock.Text = "Baku";
-            london_clock.Text = "London";
-            baku_clock.Font = new Font("Italic", 14);
-            london_clock.Font = new Font("Italic", 14);
-            baku_clock.Click += Baku_clock_Click;
-            london_clock.Click += London_clock_Click;
-            Controls.Add(baku_clock);
-            Controls.Add(london_clock);
+            Button baku_clock_button = new Button();
+            Button london_clock_button = new Button();
+            baku_clock_button.Size = new Size(85, 30);
+            london_clock_button.Size = new Size(85, 30);
+            baku_clock_button.Location = new Point(250, 150);
+            london_clock_button.Location = new Point(400, 150);
+            baku_clock_button.BackColor = Color.LightBlue;
+            london_clock_button.BackColor = Color.LightBlue;
+            baku_clock_button.Text = "Baku";
+            london_clock_button.Text = "London";
+            baku_clock_button.Font = new Font("Italic", 14);
+            london_clock_button.Font = new Font("Italic", 14);
+            baku_clock_button.Click += Baku_clock_Click;
+            london_clock_button.Click += London_clock_Click;
+            Controls.Add(baku_clock_button);
+            Controls.Add(london_clock_button);
             MyClockLabel = clocklabel;
-            Controls.Add(clocklabel);
-            
-        }       
+            Controls.Add(clocklabel);        
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            baku_time = DateTime.Now;
+            baku_time.AddSeconds(1);
+            MyClockLabel.Text = baku_time.ToLongTimeString();
+        }
         private void London_clock_Click(object sender, EventArgs e)
         {
+            london_time = baku_time.AddHours(-4);
             MyClockLabel.Text = london_time.ToLongTimeString();
-
             MyClockLabel.Image = Image.FromFile("london.png");
         }
-
         private void Baku_clock_Click(object sender, EventArgs e)
         {         
             MyClockLabel.Text = baku_time.ToLongTimeString();
